dfa8e9be0ff31c3dd54761853bcad9622684766f
BLYTHE_DELL\blyth <blythewatsonbeingessner@gmail.com>
2025-10-15 23:27:22 -0400

    set up the scene and made some floats.

--- Assets/Scripts/Coffee Pot.cs
+++ Assets/Scripts/Coffee Pot.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CoffeePot : MonoBehaviour
 {
+    public float t;
+    public float brewingTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
